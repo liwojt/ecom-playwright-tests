545bc2f8d9db411db540febbd5906a436c0abf47
Lidia Wójtowicz <lidia.wojtowicz@comarch.com>
2023-09-10 21:06:19 +0200

    Add register account page

---
+++ pages/register-account-page.js
@@ -0,0 +1,38 @@
+exports.RegisterAccountPage = class RegisterAccountPage {
+  constructor(page) {
+    this.page = page;
+    this.firstNameInput = page.getByPlaceholder('First Name');
+    this.lastNameInput = page.getByPlaceholder('Last Name');
+    this.emailInput = page.getByPlaceholder('E-mail');
+    this.telephoneInput = page.getByPlaceholder('Telephone');
+    this.passwordInput = page.getByPlaceholder('Password');
+    this.passwordConfirmInput = page.getByPlaceholder('Password Confirm');
+    this.subscribeNewsletterSelect = page.getByPlaceholder('radio');
+    this.privacyPolicyCheckbox = page.getByRole('checkbox', { name: 'Agree' });
+    this.continueButton = page.getByRole('submit', { value: 'Continue' });
+  }
+
+  async fillYourPersonalDetails(firstName, lastName, email, telephone) {
+    await this.firstNameInput.fill(firstName);
+    await this.lastNameInput.fill(lastName);
+    await this.emailInput.fill(email);
+    await this.telephoneInput.fill(telephone);
+  }
+
+  async fillYourPassword(password) {
+    await this.passwordInput.fill(password);
+    await this.passwordConfirmInput.fill(password);
+  }
+
+  async subscribeNewsletter() {
+    await this.subscribeNewsletterSelect.filter({ text: 'Yes' }).check();
+  }
+
+  async checkAgreement() {
+    await this.privacyPolicyCheckbox.check();
+  }
+
+  async clickContinueButton() {
+    await this.continueButton.click();
+  }
+};
