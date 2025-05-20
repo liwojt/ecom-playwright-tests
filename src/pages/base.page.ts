b33ca1a0733352b162baef311f92eb9b3997ee83
Lidia <liwojt@gmail.com>
2025-05-20 15:20:28 +0200

    feat: add methods to basePage

--- src/pages/base.page.ts
+++ src/pages/base.page.ts
@@ -1,5 +1,14 @@
 import { Page } from '@playwright/test';
 
 export class BasePage {
+  url = '';
   constructor(protected page: Page) {}
+
+  async navigateTo(): Promise<void> {
+    await this.page.goto(this.url);
+  }
+
+  async waitForPageLoad(): Promise<void> {
+    await this.page.waitForURL(this.url);
+  }
 }
